6b39eea8e350b7b022c044f26f77e480c9404b84
Brian Walsh <bwalsh5@elon.edu>
2024-01-03 18:27:43 -0500

    cache

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+from redis import asyncio as aioredis
 
 from .nytimes_client import get_top_stories
 from .story_formatter import format_stories_to_string
@@ -12,22 +16,28 @@ from .summarizer import summarize_news_stories
 
 app = FastAPI()
 
-from fastapi.middleware.cors import CORSMiddleware
+origins = [
+    "http://localhost:8080",
+    "http://127.0.0.1:5500/",  # Live server default
+    "https://summer-ui.fly.dev/",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[origins],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 def index():
-    return {"msg": "Welcome to my app"}
+    return {"msg": "Welcome to the News App"}
 
 
 @app.get("/news")
+@cache(namespace="test", expire=21600)
 def news():
     summary = ""
     images = []
@@ -36,6 +46,7 @@ def news():
         for story in stories:
             images.extend(story["multimedia"])
         summary = summarize_news_stories(format_stories_to_string(stories))
+        print(summary)
         images = list(
             filter(lambda image: image["format"] == "Large Thumbnail", images)
         )
@@ -45,3 +56,10 @@ def news():
             status_code=500, detail="Apologies, something bad happened :("
         )
     return {"summary": summary, "images": images}
+
+
+@app.on_event("startup")
+async def startup():
+    REDIS_URL = os.getenv("REDIS_URL", "redis://redis")
+    redis = aioredis.from_url(REDIS_URL)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
